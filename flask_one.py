d8c7ebed2e88da049245271f2824c75c833edfc2
sachin yadav <sachinyadav3496@gmail.com>
2020-06-23 10:19:39 +0530

    final touch to flask app

--- flask_one.py
+++ flask_one.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1 style='color:red'> Welcome to first Web Page Powered by Falsk<h1> <a href='/hello'>Hello</a>"
-
+    return render_template('index.html')
 @app.route("/hello/")
 def hello():
     return "<h1 style='color:blue'>Hello World</h1><a href='/'>HOME</a>"
