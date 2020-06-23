4bb5e4f119941760745838e8e4f36b3133e01cdf
sachin3496 <45626100+sachin3496@users.noreply.github.com>
2020-06-23 11:39:08 +0530

    i have made add.py more beautiful

--- add.py
+++ add.py
@@ -3,9 +3,15 @@ def add(x, y):
 
 
 if __name__ == "__main__":
-
+    print("*"*30)
+    print("*"*30)
+    print()
     x = int(input("Enter x: "))
     y = int(input("Enter y: "))
 
     r = x + y
     print(f"Result {x} + {y} = {x + y}")
+    print("")
+    print("*"*30)
+    print("*"*30)
+    
